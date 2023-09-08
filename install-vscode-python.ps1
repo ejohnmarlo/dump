01efb4a371d91905688a88ea1c1a35c96993cc83
John Marlo Evangelista <ejohnmarlo@gmail.com>
2023-09-08 11:08:08 +0800

    Update install-vscode-python.ps1

--- install-vscode-python.ps1
+++ install-vscode-python.ps1
@@ -2,11 +2,16 @@ Write-Host "Installing vscode to all users..."
 winget uninstall vscode
 winget install vscode --source=winget --scope=machine
 
+Write-Host "Installing Python 3.10 to all users..."
+winget uninstall Python.Python.3.10
+winget install Python.Python.3.10 --source=winget --scope=machine
+
 Write-Host "Installing Python 3.11 to all users..."
 winget uninstall Python.Python.3.11
 winget install Python.Python.3.11 --source=winget --scope=machine
 
 Write-Host "Changing Windows permission to allow multi-user access of Python..."
+icacls "C:\Program Files\Python310" /grant Users:f /t /q
 icacls "C:\Program Files\Python311" /grant Users:f /t /q
 
 Write-Host "Downloading sample Jupyter notebook located at C:\"
