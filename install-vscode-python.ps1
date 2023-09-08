dc61e4d07164aa332f965ef6ac4201a35411b34e
John Marlo Evangelista <ejohnmarlo@gmail.com>
2023-09-08 10:57:09 +0800

    Update install-vscode-python.ps1
    Added sample notebook for testing

--- install-vscode-python.ps1
+++ install-vscode-python.ps1
@@ -8,3 +8,6 @@ winget install Python.Python.3.11 --source=winget --scope=machine
 
 Write-Host "Changing Windows permission to allow multi-user access of Python..."
 icacls "C:\Program Files\Python311" /grant Users:f /t /q
+
+Write-Host "Downloading sample Jupyter notebook located at C:\"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/guipsamora/pandas_exercises/master/01_Getting_%26_Knowing_Your_Data/Chipotle/Exercise_with_Solutions.ipynb -OutFile C:\test.ipynb
