3173fa234e606e7c624ca4ce88fd0ae22c25bda5
John Marlo Evangelista <ejohnmarlo@gmail.com>
2023-09-13 19:54:21 +0800

    Update install-vscode-python.ps1

--- install-vscode-python.ps1
+++ install-vscode-python.ps1
@@ -16,3 +16,9 @@ icacls "C:\Program Files\Python311" /grant Users:f /t /q
 
 Write-Host "Downloading sample Jupyter notebook located at C:\"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/guipsamora/pandas_exercises/master/01_Getting_%26_Knowing_Your_Data/Chipotle/Exercise_with_Solutions.ipynb -OutFile C:\test.ipynb
+
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+
+New-NetFirewallRule -DisplayName 'Allow SSH' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22
