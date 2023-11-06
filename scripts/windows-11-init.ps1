aaff61c838c9dd081ed57dc58dc4a1db31716f36
John Marlo Evangelista <ejohnmarlo@gmail.com>
2023-11-06 11:21:05 +0800

    Update windows-11-init.ps1

--- scripts/windows-11-init.ps1
+++ scripts/windows-11-init.ps1
@@ -43,5 +43,5 @@ Start-Service sshd
 Set-Service -Name sshd -StartupType 'Automatic'
 
 New-NetFirewallRule -DisplayName 'Allow SSH' -Direction Inbound -Action Allow -Protocol TCP -LocalPort 22
-
+Get-WindowsCapability -Online | Where-Object Name -like 'OpenSSH*'
 #Start-Process 'C:\pGinaSetup-3.9.9.12.exe' -Argument '/S /D=C:\Program Files\pGina.fork'
