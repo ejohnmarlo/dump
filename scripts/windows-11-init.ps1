e5a18b00f48dc4b9c99c712429532dea18b75fdb
John Marlo Evangelista <ejohnmarlo@gmail.com>
2024-08-27 15:19:09 +0800

    Update windows-11-init.ps1

--- scripts/windows-11-init.ps1
+++ scripts/windows-11-init.ps1
@@ -159,13 +159,13 @@ winget install --id Microsoft.Powershell --source winget --scope=machine
 Write-Host "Installing Python 3.10 to all users..."
 #winget uninstall Python.Python.3.10
 #winget install Python.Python.3.10 --source=winget --scope=machine
-choco uninstall Python310
+#choco uninstall Python310
 choco install Python310 --params "/ALLUSERS"
 
 Write-Host "Installing Python 3.11 to all users..."
 #winget uninstall Python.Python.3.11
 #winget install Python.Python.3.11 --source=winget --scope=machine
-choco uninstall Python311
+#choco uninstall Python311
 choco install Python311 --params "/ALLUSERS"
 
 Write-Host "Changing Windows permission to allow multi-user access of Python..."
